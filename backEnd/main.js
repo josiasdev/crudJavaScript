c0e0c32d22f1504d9e39b2fa541a739f7ee0e5a3
josiasdev <josiasmartins098@gmail.com>
2024-11-09 14:36:27 -0300

    feat: create local storage feito

--- backEnd/main.js
+++ backEnd/main.js
@@ -9,6 +9,29 @@ const closeModal = () => document.getElementById('modal')
 
 
 
+const tempClient = {
+    nome: "Francisco Josias Da Silva Batista",
+    email: "josiasmartins098@gmail.com",
+    celular: "(85) 98231-7976",
+    cidade: "Quixadá",
+    estado: "Ceará"
+}
+
+const getLocalStorage = () => {
+    JSON.parse(localStorage.getItem('db_client')) ?? [];
+}
+
+const setLocalStorage = (dbClient) => {
+    localStorage.setItem("db_client", JSON.stringify(dbClient));
+}
+
+// CRUD - CREATE
+const createClient = (client) => {
+    const dbClient = getLocalStorage();
+    dbClient.push (client);
+    setLocalStorage(dbClient);
+}
+
 // Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
